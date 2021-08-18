32ba63482b4d288ea3a7edff8c0ce38726171b0d
aokad <aokada@ncc.go.jp>
2021-08-18 16:12:42 +0900

    add "read_len_not_quality_len"

--- scripts/otomo/reduction.py
+++ scripts/otomo/reduction.py
@@ -1,35 +1,37 @@
 import shutil
 import os
 import glob
+import re
 import otomo.CONFIG
 import otomo.analysis_status
 
 STOP_REASON =  {
     "sra_fastq_dump": {
-        "item not found": "item not found while constructing within virtual database module",
-        "cmn_iter_open_db": "cmn_iter.c cmn_iter_open_db().VDBManagerOpenDBRead",
-        "removed": "object has been removed from distribution ( 403 )",
-        "wide_character": "Wide character in subroutine entry at /usr/bin/fasterq-dump line 764.",
-        "malformed": "malformed UTF-8 character in JSON string, at character offset",
-        "parser_error": "Entity: line 1: parser error :",
-        "not_available_location": "The object is not available from your location. ( 406 )",
-        "name_not_found": "name not found while resolving query within virtual file system module - failed to resolve accession",
-        "cmn_read_String": "err: cmn_iter.c cmn_read_String",
-        "permission_denied": "unknown while reading file within file system module - unknown system error 'Permission denied(13)'",
-        "file_not_exist": "err: extract_acc2",
-        "read_len_zero": "err: sorter.c producer_thread_func: rec.read.len = 0",
-        "doesnt_exist": "no such file or directory",
-        "invalid_accession": "err: invalid accession",
+        "item not found": ["str", "item not found while constructing within virtual database module"],
+        "cmn_iter_open_db": ["str", "cmn_iter.c cmn_iter_open_db().VDBManagerOpenDBRead"],
+        "removed": ["str", "object has been removed from distribution ( 403 )"],
+        "wide_character": ["str", "Wide character in subroutine entry at /usr/bin/fasterq-dump line 764."],
+        "malformed": ["str", "malformed UTF-8 character in JSON string, at character offset"],
+        "parser_error": ["str", "Entity: line 1: parser error :"],
+        "not_available_location": ["str", "The object is not available from your location. ( 406 )"],
+        "name_not_found": ["str", "name not found while resolving query within virtual file system module - failed to resolve accession"],
+        "cmn_read_String": ["str", "err: cmn_iter.c cmn_read_String"],
+        "permission_denied": ["str", "unknown while reading file within file system module - unknown system error 'Permission denied(13)'"],
+        "file_not_exist": ["str", "err: extract_acc2"],
+        "read_len_zero": ["str", "err: sorter.c producer_thread_func: rec.read.len = 0"],
+        "doesnt_exist": ["str", "no such file or directory"],
+        "invalid_accession": ["str", "err: invalid accession"],
+        "read_len_not_quality_len": ["re", r"err: row #.+ : READ.len\([0-9]+\) != QUALITY.len\([0-9]+\) \(D\)"],
     },
     "star_alignment": {
-        "short_read": "EXITING because of FATAL ERROR in reads input: short read sequence line: ",
-        "not_consistent": "EXITING because of FATAL ERROR: Read1 and Read2 are not consistent, reached the end of the one before the other one",
-        "not_equal": "EXITING because of FATAL ERROR in reads input: quality string length is not equal to sequence length",
-        "SJ_output_is_too_small": "EXITING because of fatal error: buffer size for SJ output is too small",
-        "unknown_file_format": "EXITING because of FATAL ERROR in input reads: unknown file format: the read ID should start with",
+        "short_read": ["str", "EXITING because of FATAL ERROR in reads input: short read sequence line: "],
+        "not_consistent": ["str", "EXITING because of FATAL ERROR: Read1 and Read2 are not consistent, reached the end of the one before the other one"],
+        "not_equal": ["str", "EXITING because of FATAL ERROR in reads input: quality string length is not equal to sequence length"],
+        "SJ_output_is_too_small": ["str", "EXITING because of fatal error: buffer size for SJ output is too small"],
+        "unknown_file_format": ["str", "EXITING because of FATAL ERROR in input reads: unknown file format: the read ID should start with"],
     },
     "expression": {
-        "zero_division": "ZeroDivisionError: float division by zero"
+        "zero_division": ["str", "ZeroDivisionError: float division by zero"],
     }
 }
 
@@ -50,7 +52,13 @@ def __stop(sample, wdir):
         return ""
     
     for key in STOP_REASON[stage]:
-        if STOP_REASON[stage][key] in log:
+        stype = STOP_REASON[stage][key][0]
+        text = STOP_REASON[stage][key][1]
+
+        if stype == "re" and re.search(text, log):
+            return "%s:%s" % (stage, key)
+
+        if stype == "str" and text in log:
             return "%s:%s" % (stage, key)
     
     return ""
